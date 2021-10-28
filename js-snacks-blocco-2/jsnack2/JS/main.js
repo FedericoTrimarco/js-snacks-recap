72e9b6c598eee9143afe4e2339ba84e6c696e9cc
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-28 15:27:03 +0200

    add js-snack-2

--- js-snacks-blocco-2/jsnack2/JS/main.js
+++ js-snacks-blocco-2/jsnack2/JS/main.js
@@ -1,2 +1,15 @@
+/*   
+     JS-SNACK-2
+     Inserisci un numero, se è pari stampa il numero,
+     se è dispari stampa il numero successivo
+ */
 
+const number = parseInt(prompt('inserisci un numero'));
 
+console.log('numero inserito:', number);
+
+ if(number % 2 !== 0){
+    console.log(number + 1);
+} else{
+    console.log(number);
+}
